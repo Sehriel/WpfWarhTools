a23149c343ccde1eaf647551823b46b0cda2bf1d
Sehriel <flow_26@hotmail.fr>
2015-06-02 21:41:41 +0100

    First check-in

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WarhRessources;
+using DAOBuilder;
+using DAOs.DAOInterface;
+
+namespace WpfWarhTools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// Pour le moment, sert de support pour les tests
+    /// des ViewModels
+    /// 
+    /// Implémentera les mêmes fonctionnalités que la 
+    /// version Winform
+    /// 
+    /// Fonctionnalités :
+    /// *Barre des menus
+    /// *Barre sur la droite (qui ne sera pas forcément
+    /// que des boutons)
+    /// *Panel affichant le contrôle en cours 
+    /// d'utilisation
+    /// 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IModel_PersoCreator Source = Builder.GetDAO();
+
+            
+            List<DAOs.DAO_Core.Talent> liste = new List<DAOs.DAO_Core.Talent>();
+            Source.LoadTalents(liste);
+
+            List<ViewTalent> listeView = new List<ViewTalent>();
+
+            foreach (DAOs.DAO_Core.Talent t in liste)
+            {
+                ViewTalent tal = new ViewTalent(t);
+                listeView.Add(tal);
+
+            }
+
+        }
+
+    }
+}
---
+++ Model/Interface_Metier.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DAOs.DAOInterface;
+using DAOBuilder;
+
+namespace ObjWarh.Access_dao
+{
+/*
+    public static class daoInterface
+    {
+        public static IModel InterfaceDB { get; set; }
+
+        public static void Init()
+        {
+            InterfaceDB = DAOBuilder.Builder.GetDAO();
+        }
+
+
+
+        #region INTERFACEDBO
+
+        //Fonction déboguage : utilise un .xml pour charger les talents en DB
+        public void DeserializeAndRecord(string pathTalents, string pathCompetences)
+        {
+            InterfaceDB.DeserializeAndRecord(pathTalents, pathCompetences);
+        }
+
+        public void Sauvegarder(Metier.Personnage perso)
+        {
+            InterfaceDB.Sauvegarder(perso);
+        }
+
+
+
+        public void ChargerDataSource(List<Metier.Talent> liste)
+        {
+            foreach (DAOWarhEntities.dto.Talent t in InterfaceDB.QueryTalents())
+                liste.Add(t);
+        }
+        public void ChargerDataSource(List<Metier.Competence> liste)
+        {
+            foreach (DAOWarhEntities.dto.Competence t in InterfaceDB.QueryCompetences())
+                liste.Add(t);
+        }
+        public void ChargerDataSource(List<Metier.Personnage> liste)
+        {
+            foreach (DAOWarhEntities.dto.Personnage t in InterfaceDB.QueryPersonnages())
+                liste.Add(t);
+        }
+
+
+        public void ChargerDataSource(List<Metier.Talent> listeTalents, List<Metier.Competence> listeComps, List<Metier.Personnage> listePersos)
+        {
+            ChargerDataSource(listeTalents); 
+            ChargerDataSource(listeComps);
+            ChargerDataSource(listePersos);
+        }
+        #endregion
+        
+    }
+
+    */
+
+}
